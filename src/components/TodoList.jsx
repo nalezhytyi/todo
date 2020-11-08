a23a0b02189fbf012dffe18953c653b3463a6af6
nalezhytyi <kawory05@gmail.com>
2020-11-08 11:30:50 +0200

    added react reveal animations

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import { Slide } from 'react-awesome-reveal';
 
 const TodoList = ({ todos }) => {
   return (
     <ul className='todo-list'>
       {todos.map((item) => (
-        <TodoItem key={item.id} {...item} />
+        <Slide direction='up' triggerOnce>
+          <TodoItem key={item.id} {...item} />
+        </Slide>
       ))}
     </ul>
   );
