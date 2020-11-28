5d3c471da26b302418f64ab83651eb9aec66d656
nalezhytyi <kawory05@gmail.com>
2020-11-28 23:44:48 +0200

    added transition-group library

--- src/context.js
+++ src/context.js
@@ -1,18 +1,11 @@
 import React, { createContext, useEffect, useReducer } from 'react';
 import reducer from './reducer';
+import dummyData from './dummyData';
 
 export const Context = createContext();
 
 const initialState =
-  localStorage.getItem('todos') == null
-    ? [
-        {
-          id: 1,
-          title: 'Добавить себе ещё проблем',
-          completed: false,
-        },
-      ]
-    : JSON.parse(localStorage.getItem('todos'));
+  localStorage.getItem('todos') == null ? dummyData : JSON.parse(localStorage.getItem('todos'));
 
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
---
+++ src/dummyData.js
@@ -0,0 +1,9 @@
+const dummyData = [
+  {
+    id: 1,
+    title: 'Добавить себе ещё проблем',
+    completed: false,
+  },
+];
+
+export default dummyData;
